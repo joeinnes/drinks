4d35f71f609ce0ee8fa956765357fe0291c7b7cd
Joe Innes <joe@joeinn.es>
2025-05-06 15:19:09 +0200

    V2 setup

--- node_modules/.pnpm/jazz-browser@0.13.17/node_modules/jazz-tools/src/implementation/inspect.ts
+++
@@ -1,2 +0,0 @@
-export const inspect = Symbol.for("nodejs.util.inspect.custom");
-export type inspect = typeof inspect;
--- src/Header.tsx
+++ src/Header.tsx
@@ -5,7 +5,7 @@ import { APPLICATION_NAME } from "./main";
 export function Header() {
   return (
     <div className="flex gap-2 justify-center items-center">
-      <img src="/beer_logo.svg" className="w-16 h-16" alt="Logo" />
+      <img src="beer_logo.svg" className="w-16 h-16" alt="Logo" />
       <h1 className="text-6xl font-bold">{APPLICATION_NAME}</h1>
     </div>
   )
